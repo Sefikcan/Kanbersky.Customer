861924e66fd14c8ea82ed81994bbc6d9832e5757
Şefik Can Kanber <sefikcankanber@gmail.com>
2020-03-15 20:12:36 +0300

    controller testleri tamamlandı.

--- Kanbersky.Customer.Tests/ControllerTests/CustomerControllerTests.cs
+++ Kanbersky.Customer.Tests/ControllerTests/CustomerControllerTests.cs
@@ -1,14 +1,15 @@
-﻿using FluentAssertions;
+﻿using AutoFixture;
+using FluentAssertions;
 using Kanbersky.Customer.Api.Controllers;
+using Kanbersky.Customer.Business.Commands;
+using Kanbersky.Customer.Business.DTO.Request;
 using Kanbersky.Customer.Business.DTO.Response;
 using Kanbersky.Customer.Business.Queries;
 using Kanbersky.Customer.Core.Results;
 using MediatR;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Moq;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -20,16 +21,44 @@ namespace Kanbersky.Customer.Tests.ControllerTests
 
         private readonly CustomersController _sut;
         private readonly Mock<IMediator> _mediator;
-        Mock<IDataResult<List<GetAllCustomerQueryResponse>>> _customerListResponse = new Mock<IDataResult<List<GetAllCustomerQueryResponse>>>();
 
+        private readonly CreateCustomerRequest _createCustomerRequest;
+        private readonly UpdateCustomerRequest _updateCustomerRequest;
+
+        readonly Mock<IDataResult<List<GetAllCustomerQueryResponse>>> _customerDataListResponse = new Mock<IDataResult<List<GetAllCustomerQueryResponse>>>();
+        private readonly Mock<List<GetAllCustomerQueryResponse>> _customerListResponse;
+
+        readonly Mock<IDataResult<GetCustomerByIdQueryResponse>> _customerDataResponse = new Mock<IDataResult<GetCustomerByIdQueryResponse>>();
+        private readonly GetCustomerByIdQueryResponse _customerResponse;
+
+        private readonly Mock<IDataResult<CreateCustomerResponse>> _createCustomerDataResponse;
+        private readonly Mock<CreateCustomerResponse> _createCustomerResponse;
+
+        private readonly Mock<IDataResult<UpdateCustomerResponse>> _updateCustomerDataResponse;
+        private readonly Mock<UpdateCustomerResponse> _updateCustomerResponse;
+
+        private readonly Mock<IResult> _result;
+
+        private readonly Fixture _fixture = new Fixture();
+        private readonly int _customerId;
 
         #endregion
 
         #region ctor
 
-        public CustomerControllerTests(Mock<IMediator> mediator)
+        public CustomerControllerTests()
         {
-            _mediator = mediator;
+            _customerId = _fixture.Create<int>();
+            _result = new Mock<IResult>();
+            _updateCustomerRequest = _fixture.Create<UpdateCustomerRequest>();
+            _createCustomerRequest = _fixture.Create<CreateCustomerRequest>();
+            _createCustomerDataResponse = new Mock<IDataResult<CreateCustomerResponse>>();
+            _createCustomerResponse = new Mock<CreateCustomerResponse>();
+            _updateCustomerDataResponse = new Mock<IDataResult<UpdateCustomerResponse>>();
+            _updateCustomerResponse = new Mock<UpdateCustomerResponse>();
+            _customerResponse = _fixture.Create<GetCustomerByIdQueryResponse>();
+            _customerListResponse = new Mock<List<GetAllCustomerQueryResponse>>();
+            _mediator = new Mock<IMediator>();
             _sut = new CustomersController(_mediator.Object);
         }
 
@@ -37,49 +66,157 @@ namespace Kanbersky.Customer.Tests.ControllerTests
 
         #region methods
 
-        //[Fact]
-        //public void GetAll_Should_Return_As_Expected()
-        //{
-        //    //Arrange
-        //    _mediator.Setup(s => s.Send(new GetAllCustomersQuery())).Returns(Task.FromResult(_customerListResponse.Object));
+        [Fact]
+        public void GetAll_Should_Return_As_200OKResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<GetAllCustomersQuery>(), default)).Returns(Task.FromResult(_customerDataListResponse.Object));
+            _customerDataListResponse.Setup(t => t.Success).Returns(true);
+            _customerDataListResponse.Setup(t => t.StatusCode).Returns(StatusCodes.Status200OK);
+            _customerDataListResponse.Setup(s => s.Data).Returns(_customerListResponse.Object);
+            //Act
+            var actual = _sut.GetAllCustomers().GetAwaiter().GetResult();
 
-        //    //Act
-        //    var actual = _sut.GetAllCustomers().GetAwaiter().GetResult();
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status200OK);
+        }
 
-        //    //Assert
-        //    actual.GetType().Should().Be(typeof(OkResult));
-        //}
+        [Fact]
+        public void GetAll_Should_Return_As_404NotFoundResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<GetAllCustomersQuery>(), default)).Returns(Task.FromResult(_customerDataListResponse.Object));
+            _customerDataListResponse.Setup(t => t.Success).Returns(false);
+            _customerDataListResponse.Setup(t => t.StatusCode).Returns(StatusCodes.Status404NotFound);
+            _customerDataListResponse.Setup(s => s.Data).Returns(_customerListResponse.Object);
+            //Act
+            var actual = _sut.GetAllCustomers().GetAwaiter().GetResult();
 
-        //[Fact]
-        //public void GetById_Should_Return_As_Expected()
-        //{
-        //    //Arrange
-        //    _mediator.Setup(s => s.Send(It.IsAny<GetCustomerByIdQuery>(new { Id = 1 }))).Returns(Task.FromResult(_customerListResponse.Object));
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status404NotFound);
+        }
 
-        //    //Act
-        //    var actual = _sut.GetAllCustomers().GetAwaiter().GetResult();
+        [Fact]
+        public void GetById_Should_Return_As_200OKResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<GetCustomerByIdQuery>(), default)).Returns(Task.FromResult(_customerDataResponse.Object));
+            _customerDataResponse.Setup(t => t.Success).Returns(true);
+            _customerDataResponse.Setup(t => t.StatusCode).Returns(StatusCodes.Status200OK);
+            _customerDataResponse.Setup(s => s.Data).Returns(_customerResponse);
 
-        //    //Assert
-        //    actual.GetType().Should().Be(typeof(OkResult));
-        //}
+            //Act
+            var actual = _sut.GetCustomerById(_customerId).GetAwaiter().GetResult();
 
-        //[Fact]
-        //public void Create_Should_Return_As_Expected()
-        //{
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status200OK);
+        }
 
-        //}
+        [Fact]
+        public void GetById_Should_Return_As_404NotFoundResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<GetCustomerByIdQuery>(), default)).Returns(Task.FromResult(_customerDataResponse.Object));
+            _customerDataResponse.Setup(t => t.Success).Returns(false);
+            _customerDataResponse.Setup(t => t.StatusCode).Returns(StatusCodes.Status404NotFound);
+            _customerDataResponse.Setup(s => s.Data).Returns(_customerResponse);
 
-        //[Fact]
-        //public void Update_Should_Return_As_Expected()
-        //{
+            //Act
+            var actual = _sut.GetCustomerById(_customerId).GetAwaiter().GetResult();
 
-        //}
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status404NotFound);
+        }
 
-        //[Fact]
-        //public void Delete_Should_Return_As_Expected()
-        //{
+        [Fact]
+        public void Create_Should_Return_As_201CreatedResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<CreateCustomerCommand>(), default)).Returns(Task.FromResult(_createCustomerDataResponse.Object));
+            _createCustomerDataResponse.Setup(t => t.Success).Returns(true);
+            _createCustomerDataResponse.Setup(t => t.StatusCode).Returns(StatusCodes.Status201Created);
+            _createCustomerDataResponse.Setup(s => s.Data).Returns(_createCustomerResponse.Object);
+            //Act
+            var actual = _sut.CreateCustomer(_createCustomerRequest).GetAwaiter().GetResult();
 
-        //}
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status201Created);
+        }
+
+        [Fact]
+        public void Create_Should_Return_As_400BadRequestResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<CreateCustomerCommand>(), default)).Returns(Task.FromResult(_createCustomerDataResponse.Object));
+            _createCustomerDataResponse.Setup(t => t.Success).Returns(false);
+            _createCustomerDataResponse.Setup(t => t.StatusCode).Returns(StatusCodes.Status400BadRequest);
+            _createCustomerDataResponse.Setup(s => s.Data).Returns(_createCustomerResponse.Object);
+            //Act
+            var actual = _sut.CreateCustomer(_createCustomerRequest).GetAwaiter().GetResult();
+
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status400BadRequest);
+        }
+
+        [Fact]
+        public void Update_Should_Return_As_200OKResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<UpdateCustomerCommand>(), default)).Returns(Task.FromResult(_updateCustomerDataResponse.Object));
+            _updateCustomerDataResponse.Setup(t => t.Success).Returns(true);
+            _updateCustomerDataResponse.Setup(t => t.StatusCode).Returns(StatusCodes.Status200OK);
+            _updateCustomerDataResponse.Setup(s => s.Data).Returns(_updateCustomerResponse.Object);
+            //Act
+            var actual = _sut.UpdateCustomer(_updateCustomerRequest).GetAwaiter().GetResult();
+
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status200OK);
+        }
+
+        [Fact]
+        public void Update_Should_Return_As_400BadRequestResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<UpdateCustomerCommand>(), default)).Returns(Task.FromResult(_updateCustomerDataResponse.Object));
+            _updateCustomerDataResponse.Setup(t => t.Success).Returns(false);
+            _updateCustomerDataResponse.Setup(t => t.StatusCode).Returns(StatusCodes.Status400BadRequest);
+            _updateCustomerDataResponse.Setup(s => s.Data).Returns(_updateCustomerResponse.Object);
+            //Act
+            var actual = _sut.UpdateCustomer(_updateCustomerRequest).GetAwaiter().GetResult();
+
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status400BadRequest);
+        }
+
+        [Fact]
+        public void Delete_Should_Return_As_204NoContentResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<DeleteCustomerCommand>(), default)).Returns(Task.FromResult(_result.Object));
+            _result.Setup(t => t.Success).Returns(true);
+            _result.Setup(t => t.StatusCode).Returns(StatusCodes.Status204NoContent);
+
+            //Act
+            var actual = _sut.DeleteCustomer(_customerId).GetAwaiter().GetResult();
+
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status204NoContent);
+        }
+
+        [Fact]
+        public void Delete_Should_Return_As_400BadRequestResult()
+        {
+            //Arrange
+            _mediator.Setup(s => s.Send(It.IsAny<DeleteCustomerCommand>(), default)).Returns(Task.FromResult(_result.Object));
+            _result.Setup(t => t.Success).Returns(false);
+            _result.Setup(t => t.StatusCode).Returns(StatusCodes.Status400BadRequest);
+
+            //Act
+            var actual = _sut.DeleteCustomer(_customerId).GetAwaiter().GetResult();
+
+            //Assert
+            actual.GetType().Should().Equals(StatusCodes.Status400BadRequest);
+        }
 
         #endregion
     }
