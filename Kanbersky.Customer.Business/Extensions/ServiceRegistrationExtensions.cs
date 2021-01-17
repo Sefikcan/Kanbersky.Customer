81bdb862636e24879bb2409671dde678fddc69dc
Şefik Can Kanber <sefikcankanber@gmail.com>
2021-01-17 16:01:46 +0300

    refactor

--- Kanbersky.Customer.Business/Extensions/ServiceRegistrationExtensions.cs
+++ Kanbersky.Customer.Business/Extensions/ServiceRegistrationExtensions.cs
@@ -1,9 +1,9 @@
 ﻿using FluentValidation;
 using Kanbersky.Customer.Business.DTO.Request;
-using Kanbersky.Customer.Business.Handlers;
 using Kanbersky.Customer.Business.Validators;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Kanbersky.Customer.Business.Extensions
 {
@@ -11,7 +11,7 @@ namespace Kanbersky.Customer.Business.Extensions
     {
         public static void RegisterHandlers(this IServiceCollection services)
         {
-            services.AddMediatR(typeof(GetAllCustomersQueryHandler), typeof(GetCustomerByIdHandlers), typeof(DeleteCustomerCommandHandler), typeof(CreateCustomerCommandHandler), typeof(UpdateCustomerCommandHandler));
+            services.AddMediatR(Assembly.GetExecutingAssembly());
         }
 
         public static void RegisterValidators(this IServiceCollection services)
